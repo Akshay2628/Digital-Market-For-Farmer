bde50568f511e5fc4bd16e628c2a4ee47d74db0b
akshay <akshaygorde27@gmail.com>
2023-08-25 13:18:52 +0530

    homeController

---
+++ Digital-Market-For-Former/src/main/java/com/app/controller/HomeController.java
@@ -0,0 +1,36 @@
+package com.app.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.app.service.CategoryService;
+import com.app.service.ProductService;
+
+
+
+@Controller
+public class HomeController {
+	@Autowired
+	CategoryService categoryService;
+	@Autowired
+	ProductService productService;
+	
+	@GetMapping({"/","/home"})
+	public String home(Model model) {
+		return "index";
+		
+		}
+	
+	@GetMapping({"/","/shop"})
+	public String shop(Model model) {
+		
+		model.addAttribute("categories",categoryService.getAllCategory());
+		model.addAttribute("products",productService.getAllProduct());
+	
+		return "shop";
+		
+	}
+
+}
